34f422e755f56ff867ee7178a4f4c5d37eec6193
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-17 15:32:32 +0200

    feat: start timer 30 second and remove numbers after 30 second

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@ let messageNumber = document.querySelector("h1");
 messageNumber.innerHTML = randomNumber;
 console.log(randomNumber);
 
-// Da lì parte un timer di 30 secondi.
+// Dichiara la variabile che gestisce i secondi
+let secondCount = 0;
 
-// Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Prendi L'elemeto da HTML
+let secondTimer = document.getElementById("second");
+
+// Da lì parte un timer di 30 secondi.
+let timer = setInterval(function(){
+    secondCount++
+    secondTimer.innerHTML = secondCount;
+}, 1000);
+
+// Dopo 30 secondi i numeri scompaiono
+setTimeout(function(){
+    messageNumber.classList.add("remove");
+    secondTimer.classList.add("remove");
+    clearInterval(timer);
+}, 30000);
+
+// e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
